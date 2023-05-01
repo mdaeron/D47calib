6394f4cafb2f13bbecdab2a7d0d3b350609c53ef
mdaeron <mathieu@daeron.fr>
2023-05-01 13:30:33 +0200

    Add calibs

--- build_calibs/save_to_calibs.py
+++ build_calibs/save_to_calibs.py
@@ -3,10 +3,12 @@ with open('../src/D47calib/calibs.py', 'w') as fid:
 	fid.write('from . import D47calib\n')
 
 	for file in [
+		'breitenbach_2018.py',
 		'peral_2018.py',
 		'jautzy_2020.py',
 		'anderson_2021_mit.py',
 		'anderson_2021_lsce.py',
+		'fiebig_2021.py',
 		]:
 	
 		with open(file) as gid:
